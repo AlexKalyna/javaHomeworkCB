6ffe61b5c46d34c0f654a2ae20c77b4bd72ce85e
AlexKalyna <56594087+AlexKalyna@users.noreply.github.com>
2021-07-11 11:25:38 +0300

    Added task3 to lesson4 Homework

---
+++ src/lesson4/task3/Player.java
@@ -0,0 +1,76 @@
+package lesson4.task3;
+//Создайте 2 интерфейса Playable и Recodable. В каждом из интерфейсов создайте по 3 метода void play() /
+//void pause() / void stop() и void record() / void pause() / void stop() соответственно.
+//Создайте производный класс Player от базовых интерфейсов Playable и Recodable.
+//Написать программу, которая выполняет проигрывание и запись.
+
+import java.util.Scanner;
+
+interface Playable {
+  void play();
+  void pause();
+  void stop();
+}
+
+interface Recordable {
+  void record();
+  void pause();
+  void stop();
+}
+
+public  class Player implements Playable,Recordable {
+  @Override
+  public void play() {
+    System.out.println("File is playing");
+  }
+
+  @Override
+  public void record() {
+    System.out.println("File is recording");
+  }
+
+  @Override
+  public void pause() {
+    System.out.println("Pause");
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("Stop");
+  }
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+  String playerAction = "";
+  System.out.println("Enter player action ('G' - play, 'P' - pause, 'S' - stop, 'R' - record: ");
+
+  playerAction = input.next();
+    while (true)
+
+    if (playerAction.equals("G") || playerAction.equals("g")) {
+      Player mp3 = new Player();
+      mp3.play();
+      break;
+    } else if (playerAction.equals("P") || playerAction.equals("p")) {
+      Player mp3 = new Player();
+      mp3.pause();
+      break;
+    } else if (playerAction.equals("S") || playerAction.equals("s")) {
+      Player mp3 = new Player();
+      mp3.stop();
+      break;
+    } else if (playerAction.equals("R") || playerAction.equals("r")) {
+      Player mp3 = new Player();
+      mp3.record();
+      break;
+    } else {System.out.println("Error!");
+  break;}
+  }
+
+}
+
+
+
+
+
+
